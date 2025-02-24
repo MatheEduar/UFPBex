ffa538e6f423e101b816a19328706b4dabfe9d20
Kalil Teotonio <80278976+Galill@users.noreply.github.com>
2025-02-24 17:26:41 -0300

    Update gerenciador.py

--- Sistema/gerenciador.py
+++ Sistema/gerenciador.py
@@ -1,21 +1,28 @@
+import usuario
+
 def cadastrar_Usuario(lista):
-    nome = input("Digite o nome do usuario: ")
+    nome = input("Digite o seu nome: ")
     nick = input("Digite o nome de usuario: ")
     email = input("Digite o email do usuario: ")
     senha = input("Digite a senha do usuario: ")
-    usuario = usuario.Usuario()
-    usuario.nome = nome
-    usuario.username = nick
-    usuario.email = email
-    usuario.senha = senha
-    lista.push(usuario)
+
+    novo_usuario = usuario.Usuario(nome, nick, email, senha)
+    lista.append(novo_usuario)
+
+    print("Usuário cadastrado com sucesso!\n")
     
 def editar_Usuario(usuario):
-    #todo
-    pass
+    print("O que deseja editar?\n1 - Username\n2 - Email\n3 - Senha")
+    opcao = input("Escolha uma opção: ")
     
+    if opcao == "1":
+        usuario.username = input("Digite o novo username: ")
+    elif opcao == "2":
+        usuario.email = input("Digite o novo email: ")
+    elif opcao == "3":
+        usuario.senha = input("Digite a nova senha: ")
+    else:
+        print("Opção inválida!")
+    
+    print("Usuário atualizado com sucesso!\n")
             
-    
-
-usuarios_cadastrados = []
-cadastrar_Usuario(usuarios_cadastrados)
