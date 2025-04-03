3b750f0502c724ab565eb44cdee7bc0e4e127258
Galill <kteotonio14@gmail.com>
2025-04-02 21:44:17 -0300

    Corrigindo mais coisas pq ta complicada a situação

--- business/controllers/controllerUsers.py
+++ business/controllers/controllerUsers.py
@@ -3,38 +3,25 @@ from service.validations import Validation
 from infra.user_dao import UserDAO
 
 class ControllerUsers:
-    def __init__(self, dao: UserDAO):  # Injeta a dependência no construtor
-        self.dao = dao
-        self.validar_usuario = Validation()
+    def init(self):
+        self.facade = FacadeSingleton()
 
     def add(self, username, password):
-        self.validarUsuario.validateUserName(username)
-        self.validarUsuario.validatePassword(password)
-
-        user = User(username, password)
-        self.dao.create_user(user)
+        self.facade.add_user(username, password)
         print(f"Usuário {username} adicionado com sucesso!")
 
     def listUser(self, username):
-        return self.dao.get_user_by_username(username)
+        return self.facade.get_user(username)
 
     def listAll(self):
-        users = self.dao.get_all_users()
+        users = self.facade.get_all_users()
         for user in users:
             print(user.username)
 
     def editUser(self, username, userChange):
-        user = self.dao.get_user_by_username(username)
-        if user:
-            user.username = userChange
-            self.dao.update_user(user)
-            print(f"Usuário {username} alterado para {userChange} com sucesso!")
-        else:
-            print("Usuário não encontrado!")
+        self.facade.update_user(username, userChange)
+        print(f"Usuário {username} alterado para {userChange} com sucesso!")
 
     def deleteUser(self, username):
-        try:
-            self.dao.delete_user(username)
-            print(f"Usuário {username} deletado com sucesso!")
-        except ValueError:
-            print("Usuário não encontrado!")
+        self.facade.delete_user(username)
+        print(f"Usuário {username} deletado com sucesso!")
