b31f6d48570b3352fd3f52ced347c75ef6fbfc0d
THURRnado <arthurvieira.trabalho@gmail.com>
2025-04-10 19:49:37 -0300

    Ajeitando logica adapter e atualizando diagrama de classes

--- business/controllers/controllerCursos.py
+++ business/controllers/controllerCursos.py
@@ -1,18 +1,17 @@
 from business.model.curso import Curso
 from infra.curso_dao import CursoDAO
 from business.controllers.repository.facadeSingleton import FacadeSingleton
-from business.report import logAdapter
-
-log = logAdapter.LoggerAdapter(use_print=True)
+from business.report import loggerAdapter
 
 class ControllerCursos:
     def __init__(self, dao: CursoDAO):
         self.facade = FacadeSingleton()
         self.dao = dao
+        self.log = loggerAdapter.LoggerAdapter(use_print=True)
 
     def add(self, nome, codigo, area, periodos, carga_horaria_total, carga_horaria_optativa, carga_horaria_minima, carga_horaria_maxima, qtd_favorito):
         self.facade.add_curso(nome, codigo, area, periodos, carga_horaria_total, carga_horaria_optativa, carga_horaria_minima, carga_horaria_maxima, qtd_favorito)
-        log.info(f"Curso {nome} adicionado com sucesso!")
+        self.log.info(f"Curso {nome} adicionado com sucesso!")
 
     def listCurso(self, codigo):
         return self.facade.get_curso(codigo)
@@ -20,12 +19,12 @@ class ControllerCursos:
     def listAll(self):
         cursos = self.facade.get_all_cursos()
         for curso in cursos:
-            log.info(f"Nome: {curso.nome}, Código: {curso.codigo}, Área: {curso.area}, Períodos: {curso.periodos}, Carga Horária Total: {curso.cargaHorariatotal}, Carga Horária Optativa: {curso.cargaHorariaOptativa}, Carga Horária Mínima: {curso.cargaHorariaMinima}, Carga Horária Máxima: {curso.cargaHorariaMaxima}, Quantidade de Favoritos: {curso.qtdFavoritos}")
+            self.log.info(f"Nome: {curso.nome}, Código: {curso.codigo}, Área: {curso.area}, Períodos: {curso.periodos}, Carga Horária Total: {curso.cargaHorariatotal}, Carga Horária Optativa: {curso.cargaHorariaOptativa}, Carga Horária Mínima: {curso.cargaHorariaMinima}, Carga Horária Máxima: {curso.cargaHorariaMaxima}, Quantidade de Favoritos: {curso.qtdFavoritos}")
 
     def editCurso(self, codigo, curso_change):
         self.facade.update_curso(codigo, curso_change)
-        log.info(f"Curso {codigo} alterado para {curso_change} com sucesso!")
+        self.log.info(f"Curso {codigo} alterado para {curso_change} com sucesso!")
 
     def deleteCurso(self, codigo):
         self.facade.delete_curso(codigo)
-        log.info(f"Curso {codigo} deletado com sucesso!")
+        self.log.info(f"Curso {codigo} deletado com sucesso!")
--- business/controllers/controllerUsers.py
+++ business/controllers/controllerUsers.py
@@ -2,18 +2,17 @@ from business.model.user import User
 from service.validations import Validation
 from infra.user_dao import UserDAO
 from business.controllers.repository.facadeSingleton import FacadeSingleton
-from business.report import logAdapter
-
-log = logAdapter.LoggerAdapter(use_print=True)
+from business.report import loggerAdapter
 
 class ControllerUsers:
     def __init__(self, dao: UserDAO):
         self.facade = FacadeSingleton()
         self.dao = dao 
+        self.log = loggerAdapter.LoggerAdapter(use_print=True)
       
     def add(self, username, password):
         self.facade.add_user(username, password)
-        log.info(f"Usuário {username} adicionado com sucesso!")
+        self.log.info(f"Usuário {username} adicionado com sucesso!")
 
     def listUser(self, username):
         return self.facade.get_user(username)
@@ -21,12 +20,12 @@ class ControllerUsers:
     def listAll(self):
         users = self.facade.get_all_users()
         for user in users:
-            log.info(user.username)
+            self.log.info(user.username)
 
     def editUser(self, username, userChange):
         self.facade.update_user(username, userChange)
-        log.info(f"Usuário {username} alterado para {userChange} com sucesso!")
+        self.log.info(f"Usuário {username} alterado para {userChange} com sucesso!")
 
     def deleteUser(self, username):
         self.facade.delete_user(username)
-        log.info(f"Usuário {username} deletado com sucesso!")
+        self.log.info(f"Usuário {username} deletado com sucesso!")
